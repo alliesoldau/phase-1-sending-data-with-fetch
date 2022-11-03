772f7fa73f2417b3efb8a094539c2831e384d072
Allie Soldau <alliesoldau@gmail.com>
2022-11-03 18:00:49 -0400

    commit tree 3400a70e0ed80bf2847bed2c0ca988646605b8ca

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    }
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(userData),
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) { return response.json(); })
+    .then(function (data) { document.body.innerHTML = data["id"]; })
+    .catch(function (error) { 
+        document.body.innerHTML = error.message; })
+}
+
+
+    
+
+  
+
